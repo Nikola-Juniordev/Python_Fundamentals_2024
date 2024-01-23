24d354613d6284deebe6bcb0e66305bc750aee7f
Nick-Juniordev <156439736+Nikola-Juniordev@users.noreply.github.com>
2024-01-23 17:55:45 +0200

    new additions

---
+++ 6.Data_types_and_variables_more_exercise/Exchange_integer.py
@@ -0,0 +1,14 @@
+inpute_one = int(input())
+inpute_two = int(input())
+value_one = inpute_one
+value_two = inpute_two
+print('Before:')
+print(f'a = {value_one}')
+print((f'b = {value_two}'))
+value_two = inpute_one
+value_one = inpute_two
+print('After:')
+print(f'a = {value_one}')
+print((f'b = {value_two}'))
+
+
